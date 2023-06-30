e945069cc0940a5df3b845ef298d9bbea2af46b9
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-06-30 11:47:57 +0530

    update

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -18,7 +18,7 @@ function Signup() {
     e.preventDefault();
   
     try {
-      const response = await axios.post('http://localhost:3000/signup', {
+      const response = await axios.post('https://deployment-practice75.onrender.com/signup', {
         firstName: fullName,
         lastName,
         email,
